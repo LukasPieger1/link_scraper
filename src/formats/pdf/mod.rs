1364f804ac681ea971f45c1b1ae6bef408f17dce
Lukas <lukas.pieger@prontoweb.de>
2024-03-22 13:55:27 +0100

    Cleanup and add tests

--- src/formats/pdf/mod.rs
+++ src/formats/pdf/mod.rs
@@ -7,33 +7,50 @@ use crate::link_extractor::find_links;
 #[derive(Error, Debug)]
 pub enum PdfExtractionError {
     #[error(transparent)]
-    MuPdfError(#[from] mupdf::Error)
+    MuPdfError(#[from] mupdf::Error),
+    #[error("The file was encrypted, but no password was given.")]
+    FileEncryptedError,
+    #[error("You tried to decrypt a non-encrypted file.")]
+    FileNotEncryptedError,
+    #[error("Given file was not a PDF.")]
+    NotAPdfError,
 }
 
-fn find_text_links(page: &Page) -> Result<Vec<String>, PdfExtractionError> {
-    Ok(
-        find_links(&page.to_text()?).iter()
-            .map(|link| link.to_string()).collect()
-    )
+/// Reads a PDF as a bytearray and extracts all links from it.
+///
+/// For encrypted files please use [`extract_links_encrypted`] instead
+pub fn extract_links(bytes: &[u8]) -> Result<Vec<String>, PdfExtractionError> {
+    extract_links_from_doc(bytes_to_pdf(bytes)?)
 }
 
-fn find_hyperlinks(page: &Page) -> Result<Vec<String>, PdfExtractionError> {
-    let mut links: Vec<String> = vec![];
-    for link in page.links()? {
-        find_links(&link.uri).iter()
-            .map(|link| link.to_string())
-            .for_each(|link| links.push(link));
+/// Like [`extract_links`] for encrypted files.
+/// Currently not working. Probably because of a bug in the mupdf package.
+///
+/// I created an <a href="https://github.com/messense/mupdf-rs/issues/82">issue</a> for it.
+/// However, I don't think it is likely to be resolved.
+pub fn extract_links_encrypted(bytes: &[u8], password: &str) -> Result<Vec<String>, PdfExtractionError> {
+    let mut doc = bytes_to_pdf(bytes)?;
+    if !doc.needs_password()? {
+        return Err(PdfExtractionError::FileNotEncryptedError);
     }
 
-    Ok(links)
+    doc.authenticate(password)?;
+    extract_links_from_doc(doc)
 }
 
-pub fn extract_links(doc: Document) -> Result<Vec<String>, PdfExtractionError> {
+fn extract_links_from_doc(doc: Document) -> Result<Vec<String>, PdfExtractionError> {
+    if !doc.is_pdf() {
+        return Err(PdfExtractionError::NotAPdfError);
+    }
+    if doc.needs_password()? {
+        return Err(PdfExtractionError::FileEncryptedError);
+    }
+
     let mut links: Vec<String> = vec![];
     for page_res in doc.pages()? {
         let page = page_res?;
-        links.append(&mut find_text_links(&page)?);
-        links.append(&mut find_hyperlinks(&page)?);
+        find_text_links(&page, &mut links)?;
+        find_hyperlinks(&page, &mut links)?;
     }
 
     links = links.iter()
@@ -44,26 +61,75 @@ pub fn extract_links(doc: Document) -> Result<Vec<String>, PdfExtractionError> {
     Ok(links)
 }
 
+/// Finds plaintext links on a page
+fn find_text_links(page: &Page, links: &mut Vec<String>) -> Result<(), PdfExtractionError> {
+    find_links(&page.to_text()?).iter()
+        .for_each(|link|
+            links.push(link.to_string()));
+    Ok(())
+}
+
+/// Finds hyperlinks on a page
+fn find_hyperlinks(page: &Page, links: &mut Vec<String>) -> Result<(), PdfExtractionError> {
+    for link in page.links()? {
+        find_links(&link.uri).iter()
+            .for_each(|link|
+                links.push(link.to_string()));
+    }
+    Ok(())
+}
+
+fn bytes_to_pdf(bytes: &[u8]) -> Result<Document, PdfExtractionError> {
+    Ok(Document::from_bytes(bytes, "test.pdf")?)
+}
 
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use itertools::Itertools;
     use std::include_bytes;
 
-    const TEST_PDF: &[u8]  = include_bytes!("../../../assets/examples/pdf/Studienbescheinigung.pdf");
-    // const TEST_PDF: &[u8] = include_bytes!("../../../assets/examples/pdf/Ticket-Uppsala-Goeteborg-3141969404.pdf");
-    // const TEST_PDF: &[u8]  = include_bytes!("../../../assets/examples/pdf/2023-09-18_11-22-41.pdf");
-    // const TEST_PDF: &[u8]  = include_bytes!("../../../assets/examples/pdf/2024-01-12_23-23-34.pdf");
-    // const TEST_PDF: &[u8]  = include_bytes!("../../../assets/examples/pdf/eng.easyroam-App_Linux_Ubuntu_v22.pdf");
-    // const TEST_PDF: &[u8]  = include_bytes!("../../../assets/examples/pdf/PDF32000_2008.pdf");
+    const NOT_A_PDF: &[u8] = include_bytes!("../../../assets/examples/pdf/test.txt");
+    const TEST_PDF: &[u8] = include_bytes!("../../../assets/examples/pdf/combined_test.pdf");
+    const BIG_PDF: &[u8] = include_bytes!("../../../assets/examples/pdf/PDF32000_2008.pdf");
+    const BIG_PDF_ENCRYPTED: &[u8] = include_bytes!("../../../assets/examples/pdf/PDF32000_2008-protected.pdf"); // pass: asdfasdf
 
     #[test]
     fn extract_links_from_pdf() {
-        let doc = Document::from_bytes(TEST_PDF, "").unwrap();
-        println!("{:?}", extract_links(doc).unwrap().iter()
-                .map(|url| url.to_string()).collect_vec()
+        assert_eq!(
+            vec!["http://plaintext.link.org", "https://www.archlou.org/archbishops-leadership-briefing-8/"],
+            extract_links(TEST_PDF).unwrap()
         )
+    }
+
+    #[test]
+    fn extract_lots_of_links_from_pdf() {
+        let links = extract_links(BIG_PDF).unwrap();
+        assert_eq!(38, links.len())
+    }
+
+    #[test]
+    fn fail_on_encrypted_without_pw() {
+        let links = extract_links(BIG_PDF_ENCRYPTED);
+        assert!(links.is_err())
+    }
+
+    #[test]
+    fn fail_on_decrypting_non_encrypted_file() {
+        let links = extract_links_encrypted(BIG_PDF, "asdfasdf");
+        assert!(links.is_err())
+    }
+
+    /// Currently failing
+    #[test]
+    fn extract_lots_of_links_from_encrypted_pdf() {
+        let links = extract_links_encrypted(BIG_PDF_ENCRYPTED, "asdfasdf").unwrap();
+        assert_eq!(38, links.len())
+    }
+
+    #[test]
+    fn fail_on_non_pdf() {
+        let error = bytes_to_pdf(NOT_A_PDF);
+        assert!(error.is_err())
     }
 }
