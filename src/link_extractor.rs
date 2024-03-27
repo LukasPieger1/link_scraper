8294ca06bd721ba2e65ce11e2625640d6f4fe9ea
Lukas <lukas.pieger@prontoweb.de>
2024-03-27 20:10:30 +0100

    add a unique_and_sort function

--- src/link_extractor.rs
+++ src/link_extractor.rs
@@ -1,3 +1,5 @@
+use std::hash::Hash;
+use itertools::{Itertools};
 use lazy_static::lazy_static;
 use linkify::LinkFinder;
 use linkify::LinkKind::{Url};
@@ -19,4 +21,12 @@ pub fn find_links(content: &str) -> Vec<&str> {
     FINDER.links(content)
         .filter(|link| link.kind().eq(&Url))
         .map(|link| link.as_str()).collect()
+}
+
+pub(crate) fn unique_and_sort<T:Hash + Ord>(arr: &[T]) -> Vec<&T> {
+    arr
+        .into_iter()
+        .sorted()
+        .dedup()
+        .collect::<Vec<_>>()
 }
