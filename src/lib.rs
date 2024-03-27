fcb3e553304e7f95373c6b5e8c918ad1e7c10dd3
Lukas <lukas.pieger@prontoweb.de>
2024-03-27 20:43:02 +0100

    Rename xml-feature to raw_text

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 #[cfg(feature = "link_extraction")]
 pub mod link_extractor;
-#[cfg(any(feature = "pdf", feature = "websites", feature = "ooxml", feature = "xml"))]
+#[cfg(any(feature = "pdf", feature = "websites", feature = "ooxml", feature = "raw_text"))]
 pub mod formats;
