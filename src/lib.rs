2e8128e0819d8e8612875767e0eedb3f23ccf85e
Lukas <lukas.pieger@prontoweb.de>
2024-03-27 20:09:44 +0100

    Extend docx-functionality to all ooxml-formats

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 #[cfg(feature = "link_extraction")]
 pub mod link_extractor;
-#[cfg(any(feature = "pdf", feature = "websites", feature = "docx", feature = "xml"))]
+#[cfg(any(feature = "pdf", feature = "websites", feature = "ooxml", feature = "xml"))]
 pub mod formats;
