13a30c53ba101f45410a121a0d6c51801deccdd9
Lukas <lukas.pieger@prontoweb.de>
2024-03-28 16:45:09 +0100

    bugfix

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@
 pub mod link_extractor;
 #[cfg(any(feature = "pdf", feature = "websites", feature = "ooxml", feature = "raw_text", feature = "rtf", feature = "odt"))]
 pub mod formats;
-#[cfg(all(feature = "link_extraction", feature = "generic_file"))]
+#[cfg(all(feature = "link_extraction", feature = "generic_file", any(feature = "pdf", feature = "websites", feature = "ooxml", feature = "raw_text", feature = "rtf", feature = "odt")))]
 pub mod generic_link_extractor;
