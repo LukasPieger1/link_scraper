4603111e0b31cd44a2c8f8ac564ec3091a894a67
Lukas <lukas.pieger@prontoweb.de>
2024-09-05 19:54:04 +0200

    feat: Update doc

--- src/formats/mod.rs
+++ src/formats/mod.rs
@@ -1,14 +1,18 @@
 #[cfg(feature = "pdf")]
 pub mod pdf;
 #[cfg(feature = "plaintext")]
+/// Any plaintext-format
 pub mod plaintext;
 #[cfg(feature = "ooxml")]
+/// .docx, .pptx, .xlsx
 pub mod ooxml;
 #[cfg(feature = "rtf")]
 pub mod rtf;
 #[cfg(feature = "odf")]
+/// .odt, .ods, .odp
 pub mod odf;
 #[cfg(any(feature = "xml", feature = "xlink"))]
+/// Also contains xml-based formats
 pub mod xml;
 #[cfg(any(feature = "odf", feature = "ooxml"))]
 mod compressed_formats_common;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
+//! This Crate's main feature are the `scrape`-functions of the different Modules.
+//!
+//! You can use those functions to extract Links and related information from a file of any supported format.
+
+/// Helper functions
 pub mod helpers;
-/// Contains format-specific scrape-functions. Prefer over ['any_format_scraper'].
+/// Contains format-specific scrape-functions. Prefer over [`any_format_scraper`].
 pub mod formats;
 #[cfg(feature = "any_format")]
+/// Use only if you're not sure what format your file will be.
 pub mod any_format_scraper;
